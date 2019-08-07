17bdb2fb327ca38145505f02500cfed283e7148a
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-07 20:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 
 
-function takeANumber(katzDeliLine, newPerson){
-	katzDeliLine.push(newPerson);
-	return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
-	
+function takeANumber(katzDeliLine, newPerson)
+{
+    katzDeliLine.push(newPerson);
+    return `Welcome, ${ newPerson}. You are number ${ katzDeliLine.length} in line.`;
+
 }
 
-  function nowServing(katzDeliLine){
-    
-  	if (!katzDeliLine || katzDeliLine.length === 0){
-		return "There is nobody waiting to be served!";
-}
-	else {
-	  var name = katzDeliLine[0];
-	  katzDeliLine.shift();
-		return `Currently serving ${name}.`;
-}
-	  
+function nowServing(katzDeliLine)
+{
+
+    if (!katzDeliLine || katzDeliLine.length === 0)
+    {
+        return "There is nobody waiting to be served!";
+    }
+    else
+    {
+        var name = katzDeliLine[0];
+        katzDeliLine.shift();
+	      var name = katzDeliLine[0]
+	      katzDeliLine = katzDeliLine.slice(1)
+        return `Currently serving ${ name}.`;
+    }
+
 }
 
 
 
 
 
-    function currentLine(line){
-		var result = `The line is currently: `;
-		if (!line || line.length === 0){
-			return 'The line is currently empty.' ;
+function currentLine(line)
+{
+    var result = `The line is currently: `;
+    if (!line || line.length === 0)
+    {
+        return 'The line is currently empty.';
+    }
+    else
+    {
+        for (let i = 0; i < line.length; i++)
+            if (i != line.length - 1)
+            {
+                result += `${ i + 1}. ${ line[i]}, `;
+            }
+            else
+            {
+                result += `${ i + 1}. ${ line[i]}`;
+            }
+        return result;
+    }
+
 }
-		else {
-			for (let i = 0; i < line.length; i++)
-				if(i != line.length - 1){
-					result += `${i+1}. ${line[i]}, `;
-                }
-				else {
-					result += `${i+1}. ${line[i]}`;
-}
-    return result ;
-}
-		
-}
+
+
+
+
+
+
 
 
 

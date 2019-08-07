bde13838e8577b7bc4b5d99fa91af4d11719b0c5
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-07 20:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
+function takeANumber(katzDeliLine, newPerson) {
 
-
-function takeANumber(katzDeliLine, newPerson)
-{
     katzDeliLine.push(newPerson);
-    return `Welcome, ${ newPerson}. You are number ${ katzDeliLine.length} in line.`;
+
+    return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
+
+
 
 }
 
-function nowServing(katzDeliLine)
-{
+var counter = 0
 
-    if (!katzDeliLine || katzDeliLine.length === 0)
-    {
+function takeANumber2(){
+
+    counter += 1;
+
+    return `Welcome, you are ticket number ${counter}`
+    
+    
+    
+
+
+}
+
+
+
+
+function nowServing(katzDeliLine) {
+
+
+
+    if (!katzDeliLine || katzDeliLine.length === 0) {
+
         return "There is nobody waiting to be served!";
+
     }
-    else
-    {
+
+    else {
+
         var name = katzDeliLine[0];
-        katzDeliLine.shift();
-	      var name = katzDeliLine[0]
-	      katzDeliLine = katzDeliLine.slice(1)
-        return `Currently serving ${ name}.`;
+
+        katzDeliLine = katzDeliLine.slice(1);
+
+        return `Currently serving ${name}.`;
+
     }
+
+
 
 }
 
@@ -29,38 +53,42 @@ function nowServing(katzDeliLine)
 
 
 
-function currentLine(line)
-{
+
+
+
+
+
+
+function currentLine(line) {
+
     var result = `The line is currently: `;
-    if (!line || line.length === 0)
-    {
+
+    if (!line || line.length === 0) {
+
         return 'The line is currently empty.';
+
     }
-    else
-    {
+
+    else {
+
         for (let i = 0; i < line.length; i++)
-            if (i != line.length - 1)
-            {
-                result += `${ i + 1}. ${ line[i]}, `;
+
+            if (i != line.length - 1) {
+
+                result += `${i + 1}. ${line[i]}, `;
+
             }
-            else
-            {
-                result += `${ i + 1}. ${ line[i]}`;
+
+            else {
+
+                result += `${i + 1}. ${line[i]}`;
+
             }
+
         return result;
+
     }
+
+
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
